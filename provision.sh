9124eed194f85f1719dc50a8c055fd7e0f0fe98c
Julien Etienne <julien.etienne@gmail.com>
2018-01-12 19:13:40 -0500

    ncurses and mkosi added

--- provision.sh
+++ provision.sh
@@ -2,6 +2,6 @@
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y --no-install-recommends bc gcc-aarch64-linux-gnu device-tree-compiler git ca-certificates build-essential libc6-dev-arm64-cross wget fakeroot debhelper gawk binfmt-support qemu qemu-user-static debootstrap vim swig python-dev
+apt-get install -y --no-install-recommends bc gcc-aarch64-linux-gnu device-tree-compiler git ca-certificates build-essential libc6-dev-arm64-cross wget fakeroot debhelper gawk binfmt-support qemu qemu-user-static debootstrap vim swig python-dev libncurses5-dev mkosi
 apt-get clean
 
